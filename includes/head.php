3c05bc3523ef20c7af4ef2515b566f6dfe61424d
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-07-15 10:20:11 +0200

    refactor: modify assets

--- includes/head.php
+++ includes/head.php
@@ -3,6 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="<?= $shop['meta_description'] ?>">
     <title><?= $shop['meta_title'] ?></title>
+    <link rel="icon" href="<?= $shop['meta_icon'] ?>" type="image/ico">
     <link rel="stylesheet" href="assets/css/main.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
