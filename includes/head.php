6505145c292a96f5a02281019cbd30b148fae1d7
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-07-15 10:36:08 +0200

    fix: description variable

--- includes/head.php
+++ includes/head.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="<?= $shop['meta_description'] ?>">
+    <meta name="description" content="<?= $shop['description'] ?>">
     <title><?= $shop['title'] ?></title>
     <link rel="icon" href="<?= $shop['icon'] ?>" type="image/ico">
     <link rel="stylesheet" href="assets/css/main.css">
