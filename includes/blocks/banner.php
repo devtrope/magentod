f1ab6176a7b5eb7710b08b856bd3d136d37433b6
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-07-15 17:03:43 +0200

    feat: define blocks to display

---
+++ includes/blocks/banner.php
@@ -0,0 +1,3 @@
+<div class="header">
+    <img src="<?= $image ?>" alt="Header image">
+</div>
--- includes/blocks/image_text.php
+++ includes/blocks/image_text.php
@@ -1,8 +1,18 @@
-<div class="grid">
-    <div class="grid__left">
-        <img src="<?= $section['image'] ?>" alt="<?= $section['image_alt'] ?>">
-    </div>
-    <div class="grid__right">
-        <div class="section-text"><?= $section['text'] ?></div>
+<div class="section">
+    <div class="section-content">
+        <?php if (isset($title)) : ?>
+            <div class="section-title"><?= $title ?></div>
+        <?php endif; ?>
+        <?php if (isset($subtitle)) : ?>
+            <div class="section-subtitle"><?= $subtitle ?></div>
+        <?php endif; ?>
+        <div class="grid">
+            <div class="grid__left">
+                <img src="<?= $image ?>" alt="<?= $image_alt ?>">
+            </div>
+            <div class="grid__right">
+                <div class="section-text"><?= $text ?></div>
+            </div>
+        </div>
     </div>
 </div>
